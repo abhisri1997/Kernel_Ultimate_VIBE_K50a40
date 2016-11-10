a93e0f9611b8f85f42ab83db9a643c6dea048cbd
GSandeep24 <sandeepg2404@gmail.com>
2016-11-10 20:40:16 +0530

    Add wifi power management control

--- drivers/misc/mediatek/connectivity/conn_soc/drv_wlan/mt_wifi/wlan/nic/nic.c
+++ drivers/misc/mediatek/connectivity/conn_soc/drv_wlan/mt_wifi/wlan/nic/nic.c
@@ -635,6 +635,9 @@
 ********************************************************************************
 */
 #include "precomp.h"
+#ifdef CONFIG_MTK_WIFI_PM_CONTROL
+#include "wlan_pms.h"
+#endif
 
 /*******************************************************************************
 *                              C O N S T A N T S
@@ -2241,6 +2244,13 @@ nicConfigPowerSaveProfile(IN P_ADAPTER_T prAdapter,
 /* prAdapter->rWlanInfo.ePowerSaveMode.ucNetTypeIndex = eNetTypeIndex; */
 /* prAdapter->rWlanInfo.ePowerSaveMode.ucPsProfile = (UINT_8)ePwrMode; */
 	prAdapter->rWlanInfo.arPowerSaveMode[eNetTypeIndex].ucNetTypeIndex = eNetTypeIndex;
+	#ifdef CONFIG_MTK_WIFI_PM_CONTROL
+	if(lock) {
+		prAdapter->rWlanInfo.arPowerSaveMode[eNetTypeIndex].ucPsProfile = (UINT_8)custPowerMode;
+	}
+	else
+#endif
+
 	prAdapter->rWlanInfo.arPowerSaveMode[eNetTypeIndex].ucPsProfile = (UINT_8) ePwrMode;
 
 	return wlanSendSetQueryCmd(prAdapter,
